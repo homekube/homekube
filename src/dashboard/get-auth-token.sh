4335fd97dbde6f6bbe731e77dce4bfe3a4ff5015
Andreas Hahn <ahahn@gmx.net>
2020-07-04 12:28:32 +0200

    Modify get-auth-token.sh

--- src/dashboard/get-auth-token.sh
+++ src/dashboard/get-auth-token.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-name=simple-user # or 'admin-user'
-namespace=kubernetes-dashboard
-token=$(microk8s.kubectl -n $namespace get secret | grep ${name}-token | cut -d " " -f1)
-echo $(microk8s.kubectl -n $namespace get secret $token -o jsonpath='{.data.token}' | base64 -d)
+echo "Read the authentication token for user ${HOMEKUBE_USER_NAME}"
+HOMEKUBE_NAMESPACE=kubernetes-dashboard
+token=$(microk8s.kubectl -n ${HOMEKUBE_NAMESPACE} get secret | grep ${HOMEKUBE_USER_NAME}-token | cut -d " " -f1)
+HOMEKUBE_USER_TOKEN=$(microk8s.kubectl -n ${HOMEKUBE_NAMESPACE} get secret $token -o jsonpath='{.data.token}' | base64 -d)

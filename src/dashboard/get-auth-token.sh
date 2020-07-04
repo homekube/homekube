e1861b1f4c334d27cf13955f4535bf3089383589
Andreas Hahn <ahahn@gmx.net>
2020-07-04 12:14:34 +0200

    Bash script does not read ~/.bashrc e.g. ~/.bash_aliases

--- src/dashboard/get-auth-token.sh
+++ src/dashboard/get-auth-token.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#! /bin/bash
 name=simple-user # or 'admin-user'
 namespace=kubernetes-dashboard
-token=$(kubectl -n $namespace get secret | grep ${name}-token | cut -d " " -f1)
-echo $(kubectl -n $namespace get secret $token -o jsonpath='{.data.token}' | base64 -d)
+token=$(microk8s.kubectl -n $namespace get secret | grep ${name}-token | cut -d " " -f1)
+echo $(microk8s.kubectl -n $namespace get secret $token -o jsonpath='{.data.token}' | base64 -d)

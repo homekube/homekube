8fed37c1202f32e30691617b0f4a3f673f5ce8ba
Andreas Hahn <ahahn@gmx.net>
2021-04-14 14:31:48 +0200

    Update config Add Raspberry fix

---
+++ src/installation/k8s/lxc/host/raspberry-boot.sh
@@ -0,0 +1,4 @@
+# Append the cgroups and swap options to the kernel command line
+# Note the space before "cgroup_enable=cpuset", to add a space after the last existing item on the line
+# NOTE without this modification kubelets won't start !
+$ sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt

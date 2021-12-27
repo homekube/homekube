def4f6f4fb55838a50d395bf4febc3cc3536d8e7
Andreas Hahn <ahahn@gmx.net>
2021-12-27 17:27:24 +0100

    Add install-all.sh Update messages

--- src/install-all.sh
+++ src/install-all.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+echo "Waiting for Microk8s ready state"
 microk8s status --wait-ready
 microk8s kubectl version --short
 microk8s enable dns rbac helm3
 
 # add default helm repo
+echo "Installing default helm stable repo"
 helm repo add stable https://charts.helm.sh/stable
 
 # install whoami
+echo "Install who-am-i demo application"
 helm repo add halkeye https://halkeye.github.io/helm-charts/
 kubectl create namespace whoami
 helm install whoami halkeye/whoami -n whoami --version 0.3.2
+kubectl scale --replicas=5 deployment.apps/whoami -n whoami
 kubectl apply -f ~/homekube/src/whoami/ingress-whoami.yaml
 
 # install metallb
+echo "Install metallb LoadBalancer"
 cd ~/homekube/src/ingress
 kubectl create namespace metallb-system
 helm repo add metallb https://metallb.github.io/metallb
@@ -22,6 +27,7 @@ helm install metallb --version=0.12.0 -n metallb-system stable/metallb
 kubectl apply -f metallb-config.yaml
 
 # install ingress
+echo "Install ingress web server"
 cd ~/homekube/src/ingress
 kubectl create namespace ingress-nginx
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
@@ -30,6 +36,7 @@ helm install nginx-helm -n ingress-nginx --version=4.0.6 \
     ingress-nginx/ingress-nginx
 
 # install dashboard
+echo "Install kubernetes dashboard"
 cd ~/homekube/src/dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/aio/deploy/recommended.yaml
 kubectl apply -f create-admin-user.yaml
@@ -40,6 +47,7 @@ source secure-dashboard.sh
 kubectl apply -f ingress-dashboard.yaml
 
 # install nfs client
+echo "Install nfs service (client needs existing server)"
 sudo apt install nfs-common -y
 
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
@@ -53,6 +61,7 @@ helm install nfs-client --version=4.0.14 \
   nfs-subdir-external-provisioner/nfs-subdir-external-provisioner
 
 # install prometheus
+echo "Install prometheus"
 kubectl create namespace prometheus
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
@@ -66,6 +75,7 @@ kubectl apply -f ~/homekube/src/prometheus/ingress-prometheus.yaml
 
 
 # install grafana
+echo "Install grafana"
 cd ~/homekube/src/grafana
 
 kubectl create namespace grafana

2fb00f41aa98cfb9c6ee38c5e46f09eb4e005d61
Andreas Hahn <ahahn@gmx.net>
2024-08-05 11:13:44 +0200

    add postgres

---
+++ src/postgres/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+kubectl get ns postgres
+if [[ $?  -eq 0 ]]; then
+  echo "Skipping installation of postgres because namespace already exists"
+  echo "If you want to reinstall execute 'kubectl delete ns postgres'"
+else
+echo "Install postgres"
+
+kubectl create namespace postgres
+
+
+
+fi

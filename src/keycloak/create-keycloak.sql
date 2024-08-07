3a92f67fe0fc96492efe940d3d58e5b70f28d7a4
Andreas Hahn <ahahn@gmx.net>
2024-08-07 14:54:15 +0200

    update keycloak db permissions

--- src/keycloak/create-keycloak.sql
+++ src/keycloak/create-keycloak.sql
@@ -7,7 +7,7 @@ GRANT keycloak_role TO keycloak;
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak_role;
 
 -- Switch to keycloak db to grant all privileges on db level to keycloak_user
-\c keycloak -U admin;
+\c keycloak;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO keycloak_role;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO keycloak_role;
 GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO keycloak_role;

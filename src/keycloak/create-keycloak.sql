d4bb1122bcfebefef1c50ecf45b4dd81fb2237ac
Andreas Hahn <ahahn@gmx.net>
2024-08-07 14:19:41 +0200

    update keycloak db permissions

--- src/keycloak/create-keycloak.sql
+++ src/keycloak/create-keycloak.sql
@@ -4,5 +4,14 @@ CREATE ROLE keycloak_role VALID UNTIL 'infinity';
 CREATE ROLE keycloak LOGIN PASSWORD '${HOMEKUBE_PG_PASSWORD}' VALID UNTIL 'infinity';
 
 GRANT keycloak_role TO keycloak;
-GRANT ALL ON SCHEMA public TO admin;
-GRANT USAGE on SCHEMA public to keycloak_role;
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak_role;
+
+-- Switch to keycloak db to grant all privileges on db level to keycloak_user
+\c keycloak -U admin;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO keycloak_role;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO keycloak_role;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO keycloak_role;
+GRANT ALL PRIVILEGES ON ALL PROCEDURES IN SCHEMA public TO keycloak_role;
+
+GRANT USAGE ON SCHEMA public TO keycloak_role;
+GRANT CREATE ON SCHEMA public TO keycloak_role;

c80052790d1295c1270239c55fed39f0537c213f
Andreas Hahn <ahahn@gmx.net>
2024-08-19 17:26:00 +0200

    updating scripts

--- src/uninstall.sh
+++ src/uninstall.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-if [[ $(kubectl get ns | grep -q "^$1") ]] ; then
-  kubectl delete ns $1
-  if [[ $? -eq 0 ]] ; then
-    echo "Namespace $1 deleted"
-  fi
-  kubectl delete $1-pv
-  if [[ $? -eq 0 ]] ; then
-    echo "Persistent volume $1 deleted"
-  fi
-fi
-
 if [[ $# -ne 1 ]]; then
   echo "Usage is: '. ./uninstall.sh <namespace> or . ./uninstall.sh all'"
   echo
   echo "List of namespaces: "
   kubectl get ns
+  exit 2
+fi
+
+if kubectl get ns | grep -q "^$1"; then
+  kubectl delete ns $1
+  if kubectl get pv | grep -q "^$1-pv" ; then
+    kubectl delete $1-pv
+  fi
 fi
 
 if [[ $1 == "all" ]]; then
-  . ./uninstall.sh grafana
+#  . ./uninstall.sh grafana
   . ./uninstall.sh ingress-nginx
-  . ./uninstall.sh keycloak
-  . ./uninstall.sh kubernetes-dashboard
-  . ./uninstall.sh nfs-storage
-  . ./uninstall.sh postgres
+#  . ./uninstall.sh keycloak
+#  . ./uninstall.sh kubernetes-dashboard
+#  . ./uninstall.sh nfs-storage
+#  . ./uninstall.sh postgres
   . ./uninstall.sh prometheus
   . ./uninstall.sh whoami
 fi
-

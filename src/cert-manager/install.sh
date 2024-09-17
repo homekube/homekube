5bc28d659924653168be6dbbf21f86575281f4b1
Andreas Hahn <ahahn@gmx.net>
2024-09-17 11:40:52 +0200

    update installation cert-manager

--- src/cert-manager/install.sh
+++ src/cert-manager/install.sh
@@ -20,30 +20,30 @@ kubectl create -f https://github.com/cert-manager/cert-manager/releases/download
 kubectl create ns cert-manager-${HOMEKUBE_DOMAIN_DASHED}
 kubectl create secret generic acme-dns-secret -n cert-manager-${HOMEKUBE_DOMAIN_DASHED} --from-file=acme-dns-secret-key=acme-dns-${HOMEKUBE_DOMAIN_DASHED}.json
 
+sleep 30s
 # Verify the setup by creating a staging (test) secret
 envsubst < staging-template.yaml | kubectl apply -f -
-echo "Waiting for the staging secret from LetsEncypt up to 120s - be patient"
+echo "Waiting for the staging secret from LetsEncypt - be patient"
 while true; do
   secret_data=$(kubectl get secret tls-staging -n cert-manager-${HOMEKUBE_DOMAIN_DASHED} -o jsonpath='{.data.tls\.crt}')
   if [[ "$secret_data" != "" ]]; then
     echo "Secret data found!"
     break
   fi
-  echo "Waiting for secret data..."
   sleep 5
 done
 kubectl describe secret tls-staging -n cert-manager-${HOMEKUBE_DOMAIN_DASHED}
 
+sleep 30s
 # Obtain the production secret from LetsEncrypt
 envsubst < prod-template.yaml | kubectl apply -f -
-echo "Waiting for the production /real) from LetsEcypt secret up to 120s - be patient"
+echo "Waiting for the production secret from LetsEcypt - be patient"
 while true; do
   secret_data=$(kubectl get secret tls-prod -n cert-manager-${HOMEKUBE_DOMAIN_DASHED} -o jsonpath='{.data.tls\.crt}')
   if [[ "$secret_data" != "" ]]; then
     echo "Secret data found!"
     break
   fi
-  echo "Waiting for secret data..."
   sleep 5
 done
 kubectl describe secret tls-prod -n cert-manager-${HOMEKUBE_DOMAIN_DASHED}
